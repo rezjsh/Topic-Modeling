9d46b8f6375778d7f9885100a4949b5ab7aba3a4
rezjsh <149146755+rezjsh@users.noreply.github.com>
2026-01-07 17:04:55 +0330

    Enhance inference pipeline with Singleton pattern and type hints
    Applied SingletonMeta to InferencePipeline for controlled instance creation.
    
    Added explicit return type hint List[dict] to run_pipeline for clarity.
    
    Updated DataTransformation initialization to use keyword argument for improved readability.

--- src/topic_modeling/pipeline/stage_10_inference.py
+++ src/topic_modeling/pipeline/stage_10_inference.py
@@ -3,19 +3,21 @@ from topic_modeling.components.data_transformation import DataTransformation
 from topic_modeling.components.inference import TopicPredictor
 from topic_modeling.config.configuration import ConfigurationManager
 from topic_modeling.utils.logging_setup import logger
+from topic_modeling.core.singleton import SingletonMeta
 
-class InferencePipeline:
+
+class InferencePipeline(metaclass=SingletonMeta):
     def __init__(self, config_manager: ConfigurationManager):
         self.config = config_manager
         self.pred_config = config_manager.get_prediction_config()
         self.trans_config = config_manager.get_data_transformation_config()
         self.factory_config = config_manager.get_model_factory_config()
-    def run_pipeline(self, user_texts: List[str]):
+    def run_pipeline(self, user_texts: List[str]) -> List[dict]:
         """
         Entry point for unseen data.
         """
         try:
-            transformation = DataTransformation(self.trans_config)
+            transformation = DataTransformation(config = self.trans_config)
 
             self.predictor = TopicPredictor(self.pred_config,self.factory_config,  transformation)
 
